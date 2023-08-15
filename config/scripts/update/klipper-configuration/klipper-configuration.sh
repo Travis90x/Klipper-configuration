3addd2e29fda4f3b7b04145a66579f953d83f186
Travis90x <travis_90@hotmail.it>
2023-08-15 13:40:13 +0200

    Update klipper-configuration.sh

--- config/scripts/update/klipper-configuration/klipper-configuration.sh
+++ config/scripts/update/klipper-configuration/klipper-configuration.sh
@@ -2,4 +2,4 @@ git clone https://github.com/Travis90x/Klipper-configuration.git
 cd ~/Klipper-configuration && git pull && cd -
 sudo chown -R pi: ~/printer_data
 cp -r ~/Klipper-configuration/* ~/printer_data/config
-sfind ~/printer_data/config/config/scripts/ -type f -name "*.sh" -exec chmod +x {} ;
+find ~/printer_data/config/config/scripts/ -type f -name "*.sh" -exec chmod +x {} ;
