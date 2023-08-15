3248013f02a6a8efe90b6010a71ffe21a5d7078b
Travis90x <travis_90@hotmail.it>
2023-08-15 12:44:51 +0200

    Create Klipperscreen_USB.sh

---
+++ config/scripts/klipperscreen/Klipperscreen_USB.sh
@@ -0,0 +1,5 @@
+sudo apt-get install android-tools-adb
+cd ~/KlipperScreen/scripts
+cp sample-android-adb.sh launch_KlipperScreen.sh
+sudo chmod +x launch_KlipperScreen.sh
+sudo service KlipperScreen restart
