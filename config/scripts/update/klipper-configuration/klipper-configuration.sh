9ce3fe5b06baeba63860140b984733fda5107531
Travis90x <travis_90@hotmail.it>
2023-08-15 13:52:21 +0200

    Update klipper-configuration.sh

--- config/scripts/update/klipper-configuration/klipper-configuration.sh
+++ config/scripts/update/klipper-configuration/klipper-configuration.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/Travis90x/Klipper-configuration.git
+echo Don't worry if already exists, it will be updated
 cd ~/Klipper-configuration && git pull && cd -
 cp -r ~/Klipper-configuration/* ~/printer_data/config
