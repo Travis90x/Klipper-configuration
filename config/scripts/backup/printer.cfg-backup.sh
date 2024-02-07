55a2ec4dfbedef57e98e2269c6a4b5e7bbbba618
Travis90x <travis_90@hotmail.it>
2024-02-07 23:14:04 +0100

    Update printer.cfg-backup.sh

--- config/scripts/backup/printer.cfg-backup.sh
+++ config/scripts/backup/printer.cfg-backup.sh
@@ -1,1 +1,6 @@
-mv -v ~/printer_data/config/printer-*.cfg ~/printer_data/config/config/backup/
+if find ~/printer_data/config -maxdepth 1 -type f -name "printer-*.cfg" | grep -q .; then
+    mv -v ~/printer_data/config/printer-*.cfg ~/printer_data/config/config/backup/
+    echo "Backups moved."
+else
+    echo "No backups to move."
+fi
