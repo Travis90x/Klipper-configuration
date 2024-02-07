abf929be53a82ab0438b33ad0c2b6d56f0cacd8b
Travis90x <travis_90@hotmail.it>
2024-02-07 22:45:11 +0100

    Update printer.cfg-backup_clean.sh

--- config/scripts/backup/printer.cfg-backup_clean.sh
+++ config/scripts/backup/printer.cfg-backup_clean.sh
@@ -1,2 +1,8 @@
 cd ~/printer_data/config/config/backup/
-ls -lt printer-*.cfg | tail -n +11 | xargs rm
+# ls -lt printer-*.cfg | tail -n +11 | xargs rm
+if [[ $(find . -maxdepth 1 -type f -name "printer-*.cfg") ]]; then
+     find . -maxdepth 1 -type f -name "printer-*.cfg" -printf '%T@ %p\n' | sort -nr | tail -n +11 | cut -d ' ' -f 2- | xargs -- rm
+     echo "Backups cleaned."
+else
+     echo "No file to clean."
+fi
