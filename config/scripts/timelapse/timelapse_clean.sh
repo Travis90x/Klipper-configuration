c0e8821853094eef6df1124364da6e17cafb496f
Travis90x <travis_90@hotmail.it>
2024-04-28 20:59:36 +0200

    Update timelapse_clean.sh

--- config/scripts/timelapse/timelapse_clean.sh
+++ config/scripts/timelapse/timelapse_clean.sh
@@ -1,1 +1,12 @@
-find /tmp/timelapse/ -name 'frame*.jpg' -size -15k -exec rm {} +
+#!/bin/sh
+dir="/tmp/timelapse/"
+find $dir -name 'frame*.jpg' -size -15k -exec rm {} +
+counter=1
+files=$(ls $dir*.jpg | sort -V)
+echo "$files" | while read file; do
+    new_file=$(printf "frame%06d.jpg" $counter)
+    if [ "$file" != "$dir$new_file" ]; then
+        mv -- "$file" "$dir$new_file"
+    fi
+    counter=$((counter+1))
+done
