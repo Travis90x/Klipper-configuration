b890da9ce161c563a613ef79434ba12cd9c30166
Travis90x <travis_90@hotmail.it>
2024-05-02 11:24:15 +0200

    Update IDM_Temp_compensation.sh

--- config/sensors/probe/IDM/IDM_Temp_compensation.sh
+++ config/sensors/probe/IDM/IDM_Temp_compensation.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-mv ~/data1/* ~/IDM
-mv ~/data2/* ~/IDM
-mv ~/data3/* ~/IDM
-mv ~/data4/* ~/IDM
+mv -f ~/klipper/data1 ~/klipper/data2 ~/klipper/data3 ~/klipper/data4 ~/IDM/ 2>/dev/null
+echo "Files moved in IDM: data1, data2, data3, data4."
 cd ~/IDM
 ~/klippy-env/bin/python arg_fit.py
+mv -f ~/IDM/fit_result.png ~/printer_data/config/IDM_result/fit_result.png 2>/dev/null
+echo "File fit_result.png moved in printer_data/config/IDM_result."
+echo "Rendering complete."
+
