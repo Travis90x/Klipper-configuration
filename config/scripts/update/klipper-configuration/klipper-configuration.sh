8826f97e316a5e7b5bbff00a725587a86ed8823c
Travis90x <travis_90@hotmail.it>
2024-05-11 20:15:35 +0200

    Update klipper-configuration.sh

--- config/scripts/update/klipper-configuration/klipper-configuration.sh
+++ config/scripts/update/klipper-configuration/klipper-configuration.sh
@@ -26,56 +26,56 @@ echo Creating Log CPU scripts
 sudo rm /usr/local/bin/LOG_CPU_START
 sudo ln -s ~/printer_data/config/config/scripts/cpu/LOG_CPU_START.sh /usr/local/bin/LOG_CPU_START
 sudo rm /usr/local/bin/LOG_CPU_STOP
-sudo ln -s /home/pi/printer_data/config/config/scripts/cpu/LOG_CPU_STOP.sh /usr/local/bin/LOG_CPU_STOP
+sudo ln -s ~/printer_data/config/config/scripts/cpu/LOG_CPU_STOP.sh /usr/local/bin/LOG_CPU_STOP
 sudo rm /usr/local/bin/LOG_CPU_CLEAN
-sudo ln -s /home/pi/printer_data/config/config/scripts/cpu/LOG_CPU_CLEAN.sh /usr/local/bin/LOG_CPU_CLEAN
+sudo ln -s ~/printer_data/config/config/scripts/cpu/LOG_CPU_CLEAN.sh /usr/local/bin/LOG_CPU_CLEAN
 
 echo Creating Input Shaping scripts
 sudo rm /usr/local/bin/generate-shaper-graph
-sudo ln -s /home/pi/printer_data/config/config/scripts/input-shaping/generate-shaper-graph.sh /usr/local/bin/generate-shaper-graph
+sudo ln -s ~/printer_data/config/config/scripts/input-shaping/generate-shaper-graph.sh /usr/local/bin/generate-shaper-graph
 sudo rm /usr/local/bin/generate-shaper-graph-x
-sudo ln -s /home/pi/printer_data/config/config/scripts/input-shaping/generate-shaper-graph-x.sh /usr/local/bin/generate-shaper-graph-x
+sudo ln -s ~/printer_data/config/config/scripts/input-shaping/generate-shaper-graph-x.sh /usr/local/bin/generate-shaper-graph-x
 sudo rm /usr/local/bin/generate-shaper-graph-y
-sudo ln -s /home/pi/printer_data/config/config/scripts/input-shaping/generate-shaper-graph-y.sh /usr/local/bin/generate-shaper-graph-y
+sudo ln -s ~/printer_data/config/config/scripts/input-shaping/generate-shaper-graph-y.sh /usr/local/bin/generate-shaper-graph-y
 sudo rm /usr/local/bin/generate-belt-tension-graph
-sudo ln -s /home/pi/printer_data/config/config/scripts/input-shaping/generate-belt-tension-graph.sh /usr/local/bin/generate-belt-tension-graph
+sudo ln -s ~/printer_data/config/config/scripts/input-shaping/generate-belt-tension-graph.sh /usr/local/bin/generate-belt-tension-graph
 
 echo Creating KlipperScreen scripts
 sudo rm /usr/local/bin/Klipperscreen_USB
-sudo ln -s /home/pi/printer_data/config/config/scripts/klipperscreen/Klipperscreen_USB.sh /usr/local/bin/Klipperscreen_USB
+sudo ln -s ~/printer_data/config/config/scripts/klipperscreen/Klipperscreen_USB.sh /usr/local/bin/Klipperscreen_USB
 sudo rm /usr/local/bin/Klipperscreen_WIFI
-sudo ln -s /home/pi/printer_data/config/config/scripts/klipperscreen/Klipperscreen_WIFI.sh /usr/local/bin/Klipperscreen_WIFI
+sudo ln -s ~/printer_data/config/config/scripts/klipperscreen/Klipperscreen_WIFI.sh /usr/local/bin/Klipperscreen_WIFI
 
 echo Creating LOGS scripts
 sudo rm /usr/local/bin/LOGS_KLIPPER_CLEAN
-sudo ln -s /home/pi/printer_data/config/config/scripts/logs/klipper/LOGS_KLIPPER_CLEAN.sh /usr/local/bin/LOGS_KLIPPER_CLEAN
+sudo ln -s ~/printer_data/config/config/scripts/logs/klipper/LOGS_KLIPPER_CLEAN.sh /usr/local/bin/LOGS_KLIPPER_CLEAN
 
 echo Creating Power Loss Recovery scripts
 sudo rm /usr/local/bin/plr
-sudo ln -s /home/pi/printer_data/config/config/scripts/power-loss-recovery/plr.sh /usr/local/bin/plr
+sudo ln -s ~/printer_data/config/config/scripts/power-loss-recovery/plr.sh /usr/local/bin/plr
 sudo rm /usr/local/bin/plr_z
-sudo ln -s /home/pi/printer_data/config/config/scripts/power-loss-recovery/plr_z.sh /usr/local/bin/plr_z
+sudo ln -s ~/printer_data/config/config/scripts/power-loss-recovery/plr_z.sh /usr/local/bin/plr_z
 
 echo Creating timelapse clean scripts
 sudo rm /usr/local/bin/timelapse_clean
-sudo ln -s /home/pi/printer_data/config/config/scripts/timelapse/timelapse_clean.sh /usr/local/bin/timelapse_clean
+sudo ln -s ~/printer_data/config/config/scripts/timelapse/timelapse_clean.sh /usr/local/bin/timelapse_clean
 
 echo Creating USB scripts
 sudo rm /usr/local/bin/USB_KEY
-sudo ln -s /home/pi/printer_data/config/config/scripts/usb/USB_KEY.sh /usr/local/bin/USB_KEY
+sudo ln -s ~/printer_data/config/config/scripts/usb/USB_KEY.sh /usr/local/bin/USB_KEY
 sudo rm /usr/local/bin/USB_MOUNT
-sudo ln -s /home/pi/printer_data/config/config/scripts/usb/USB_MOUNT.sh /usr/local/bin/USB_MOUNT
+sudo ln -s ~/printer_data/config/config/scripts/usb/USB_MOUNT.sh /usr/local/bin/USB_MOUNT
 sudo rm /usr/local/bin/USB_UNMOUNT
-sudo ln -s /home/pi/printer_data/config/config/scripts/usb/USB_UNMOUNT.sh /usr/local/bin/USB_UNMOUNT
+sudo ln -s ~/printer_data/config/config/scripts/usb/USB_UNMOUNT.sh /usr/local/bin/USB_UNMOUNT
 sudo rm /usr/local/bin/USB_COPY
-sudo ln -s /home/pi/printer_data/config/config/scripts/usb/USB_COPY.sh /usr/local/bin/USB_COPY
+sudo ln -s ~/printer_data/config/config/scripts/usb/USB_COPY.sh /usr/local/bin/USB_COPY
 sudo rm /usr/local/bin/USB_Check
-sudo ln -s /home/pi/printer_data/config/config/scripts/usb/USB_Check.sh /usr/local/bin/USB_Check
+sudo ln -s ~/printer_data/config/config/scripts/usb/USB_Check.sh /usr/local/bin/USB_Check
 
 echo Creating WIFI scripts
 sudo rm /usr/local/bin/Change_WIFI
-sudo ln -s /home/pi/printer_data/config/config/scripts/wifi/Change_WIFI.sh /usr/local/bin/Change_WIFI
+sudo ln -s ~/printer_data/config/config/scripts/wifi/Change_WIFI.sh /usr/local/bin/Change_WIFI
 sudo rm /usr/local/bin/Delete_WIFI
-sudo ln -s /home/pi/printer_data/config/config/scripts/wifi/Delete_WIFI.sh /usr/local/bin/Delete_WIFI
+sudo ln -s ~/printer_data/config/config/scripts/wifi/Delete_WIFI.sh /usr/local/bin/Delete_WIFI
 sudo rm /usr/local/bin/Show_WIFI
-sudo ln -s /home/pi/printer_data/config/config/scripts/wifi/Show_WIFI.sh /usr/local/bin/Show_WIFI
+sudo ln -s ~/printer_data/config/config/scripts/wifi/Show_WIFI.sh /usr/local/bin/Show_WIFI
