d98e0bbd50bf3f50611a2330aec855bf7683bf2d
Travis90x <travis_90@hotmail.it>
2024-05-11 19:43:14 +0200

    Update klipper-configuration.sh

--- config/scripts/update/klipper-configuration/klipper-configuration.sh
+++ config/scripts/update/klipper-configuration/klipper-configuration.sh
@@ -1,3 +1,8 @@
 if [ ! -d ~/Klipper-configuration/ ]; then git clone https://github.com/Travis90x/Klipper-configuration.git ~/Klipper-configuration/; fi
-cd ~/Klipper-configuration && git pull && cd -
+cd ~/Klipper-configuration && git pull --rebase && cd -
 cp -r ~/Klipper-configuration/* ~/printer_data/config
+sudo chown -R $USER: ~/printer_data
+sudo find ~/printer_data/config/config/scripts/ -type f -name "*.sh" -exec chmod +x {} \;
+sudo ln -s ~/printer_data/config/config/scripts/webcam/webcam_check.sh /usr/local/bin/webcam_check
+sudo ln -s ~/printer_data/config/config/scripts/webcam/webcam_start.sh /usr/local/bin/webcam_start
+sudo ln -s ~/printer_data/config/config/scripts/webcam/webcam_stop.sh  /usr/local/bin/webcam_stop
