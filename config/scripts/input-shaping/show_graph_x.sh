adba23ae0900bc79abcb0484442b481f9819e781
Travis90x <travis_90@hotmail.it>
2024-11-23 19:48:57 +0100

    Update show_graph_x.sh

--- config/scripts/input-shaping/show_graph_x.sh
+++ config/scripts/input-shaping/show_graph_x.sh
@@ -4,6 +4,6 @@ OUTLINK=../server/files/config/input_shaper/1_shaper_calibrate_x.png
 
 title='<b><p style="font-weight-bold; margin:0; color:white">FILE:'${OUTFILE}'</p></b>'
 info="<a style=color:cyan>Click image to download<a style=color:#fb15ff> or right click for options.</a></a>"
-img='<a href="'${OUTFILE}'" target="_blank" ><img src="'${OUTFILE}'" width="100%"></a>'
+img='<a href="'${OUTLINK}'" target="_blank" ><img src="'${OUTLINK}'" width="100%"></a>'
 console_out="$title\n$info\n$img"
 echo -e "$console_out"
