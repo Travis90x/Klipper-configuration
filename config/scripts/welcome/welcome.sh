8f97512f8e8123aee171c2da0f1b8d8e603e0fe1
Travis90x <travis_90@hotmail.it>
2024-11-23 19:19:48 +0100

    Create welcome.sh

---
+++ config/scripts/welcome/welcome.sh
@@ -0,0 +1,5 @@
+title='<b><p style="font-weight-bold; margin:0; color:white">Loading...</p></b>'
+info="<a style=color:cyan>Welcome <a style=color:#fb15ff> in Klipper 3D.</a></a>"
+img='<a href="../server/files/config/welcome.png" target="_blank" ><img src="../server/files/config/welcome.png" width="100%"></a>'
+console_out="$title\n$info\n$img"
+echo -e "$console_out"
