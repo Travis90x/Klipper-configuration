39e881177dcac70999645a15848caa44be3e8675
Travis90x <travis_90@hotmail.it>
2024-12-16 22:54:52 +0100

    Update welcome.sh

--- config/scripts/welcome/welcome.sh
+++ config/scripts/welcome/welcome.sh
@@ -1,5 +1,5 @@
-title='<b><p style="font-weight-bold; margin:0; color:white">Loading...</p></b>'
-info="<a style=color:cyan>Welcome <a style=color:#fb15ff> in Klipper 3D.</a></a>"
+# title='<b><p style="font-weight-bold; margin:0; color:white">Ready!</p></b>'
+info="<a style=color:cyan>Welcome<a style=color:#fb15ff> in Klipper 3D</a></a>"
 img='<a href="../server/files/config/welcome.png" target="_blank" ><img src="../server/files/config/welcome.png" width="25%"></a>'
 console_out="$title\n$info\n$img"
 echo -e "$console_out"
