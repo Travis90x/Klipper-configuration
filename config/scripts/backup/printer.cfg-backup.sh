b19fd66211b4cceb8b64563ebf64a344f63a07f9
Travis90x <travis_90@hotmail.it>
2025-02-06 19:12:27 +0100

    Update printer.cfg-backup.sh

--- config/scripts/backup/printer.cfg-backup.sh
+++ config/scripts/backup/printer.cfg-backup.sh
@@ -1,5 +1,6 @@
 if find ~/printer_data/config -maxdepth 1 -type f -name "printer-*.cfg" | grep -q .; then
     mv -v ~/printer_data/config/printer-*.cfg ~/printer_data/config/config/backup/
+    mv -v ~/printer_data/config/moonraker.conf.backup ~/printer_data/config/config/backup/
     echo "Backups moved."
 else
     echo "No backups to move."
