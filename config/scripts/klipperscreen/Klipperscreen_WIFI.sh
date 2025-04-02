25c0953dca2fb88e2a1ede02104ec14e4191459a
Travis90x <travis_90@hotmail.it>
2025-04-02 19:39:03 +0200

    Update Klipperscreen_WIFI.sh

--- config/scripts/klipperscreen/Klipperscreen_WIFI.sh
+++ config/scripts/klipperscreen/Klipperscreen_WIFI.sh
@@ -1,10 +1,9 @@
-sudo apt-get install android-tools-adb
+echo "$1" | sudo apt-get install android-tools-adb
 cd ~/KlipperScreen/scripts
 rm launch_KlipperScreen.sh
+cp sample-android-adb.sh launch_KlipperScreen.sh
+sudo chmod +x launch_KlipperScreen.sh
 touch launch_KlipperScreen.sh
-chmod +x launch_KlipperScreen.sh
-
-# ${1} is your IP from the macro
-echo 'DISPLAY=${1}:0 $KS_XCLIENT' > /home/pi/KlipperScreen/scripts/launch_KlipperScreen.sh
-
+# ${2} is your IP from the macro
+echo 'DISPLAY=${2}:0 $KS_XCLIENT' > /home/pi/KlipperScreen/scripts/launch_KlipperScreen.sh
 sudo service KlipperScreen restart
