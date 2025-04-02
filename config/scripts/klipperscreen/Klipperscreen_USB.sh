87434ce4cfd7a1c1570a007f513192766138a7e9
Travis90x <travis_90@hotmail.it>
2025-04-02 19:39:16 +0200

    Update Klipperscreen_USB.sh

--- config/scripts/klipperscreen/Klipperscreen_USB.sh
+++ config/scripts/klipperscreen/Klipperscreen_USB.sh
@@ -1,5 +1,6 @@
-sudo apt-get install android-tools-adb
+echo "$1" | sudo apt-get install android-tools-adb
 cd ~/KlipperScreen/scripts
+rm launch_KlipperScreen.sh
 cp sample-android-adb.sh launch_KlipperScreen.sh
 sudo chmod +x launch_KlipperScreen.sh
 sudo service KlipperScreen restart
